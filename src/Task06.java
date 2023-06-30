03400cab9a11bc0a43aaa273548c3d68ec685f5d
flint <flint.979@gmail.com>
2023-06-30 20:20:29 +0300

    30-06-23 (task06)

--- src/Task06.java
+++ src/Task06.java
@@ -1,31 +1,26 @@
 public class Task06 {
     public static void main (String[] args) {
         for (int i=10000000;i<100000000;i++){
-            int step = 0;
-            int number=i;
-            int numberTemp = number;
+             int number=i;
+            int numberTemp = 0;
             boolean isUnicue = true;
             while(number>0 && isUnicue){
-                int tempNumber = number%10;
-                int stepTemp = 0;
-                numberTemp = i;
+                int tempDigit = number%10;
+                numberTemp = number/10;
                 while(numberTemp>0 && isUnicue){
-                    int tempNumber1 = numberTemp%10;
-                    if(stepTemp==step) {
-                    numberTemp/=10;
-                    stepTemp++;
-                    continue;}
-                    if (tempNumber1==tempNumber) {
+                    if (numberTemp%10==tempDigit){
                         isUnicue = false;
                         break;
                     }
                     numberTemp/=10;
-                    stepTemp++;
                 }
+                if(isUnicue==false) break;
                 number/=10;
-                step++;
             }
+
             if(isUnicue && i%12345==0) System.out.println(i);
+
         }
+
     }
 }
